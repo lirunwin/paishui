55ec6e1e8553d2af07c59d70cf1de59ded56981e
李顺 <876330731@qq.com>
2022-02-18 17:04:13 +0800

    Item.vue复原

--- src/layout/components/Sidebar/Item.vue
+++ src/layout/components/Sidebar/Item.vue
@@ -1,32 +1,32 @@
-<script lang='ts'>
-import { CreateElement } from "vue";
-import { Vue, Component, Prop } from "vue-property-decorator";
-@Component({
-  name: "MenuItem",
-})
-export default class Item extends Vue {
-  functional = true;
-  @Prop({ default: "" }) icon: string;
-  @Prop({ default: "" }) title: string;
-  render(createElement:CreateElement, context){
-    const { icon, title } = context.props;
-    const vnodes = [];
-    //使用createElement创建元素
+<script>
+export default {
+  name: 'MenuItem',
+  functional: true,
+  props: {
+    icon: {
+      type: String,
+      default: ''
+    },
+    title: {
+      type: String,
+      default: ''
+    }
+  },
+  render(h, context) {
+    const { icon, title } = context.props
+    const vnodes = []
+
     if (icon) {
       // vnodes.push(<svg-icon icon-class={icon}/>)
       // vnodes.push(<i style='color:rgb(147,177,211)' class={icon}></i>)
-      const ehtml=createElement('i',{attrs:{'class':icon,'style':'color: "#3b3b3b;'}})
-      //vnodes.push(<i class={icon} style='color: "#3b3b3b;'></i>);
+      vnodes.push(<i class={icon} style='color: "#3b3b3b;'></i>)
       // <i class="el-icon-edit"></i>
-      vnodes.push(ehtml)
     }
 
     if (title) {
-      const ehtml=createElement('span',{attrs:{'slot':'title'}},title)
-      //vnodes.push(<span slot="title">{title}</span>);
-      vnodes.push(ehtml)
+      vnodes.push(<span slot='title'>{(title)}</span>)
     }
-    return vnodes;
+    return vnodes
   }
 }
 </script>
