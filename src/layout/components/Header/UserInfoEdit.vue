eeeece18486a4a5e9561ab64e2017e4a07a35e87
LR <lr@somemailserver.com>
2022-03-31 11:28:54 +0800

    fix: 个性签名

--- src/layout/components/Header/UserInfoEdit.vue
+++ src/layout/components/Header/UserInfoEdit.vue
@@ -4,8 +4,8 @@
     :visible="showApply"
     width="800px"
     append-to-body
-    @close="closeDialog"
     :close-on-click-modal="false"
+    @close="closeDialog"
   >
     <el-form
       ref="ruleForm"
@@ -73,8 +73,9 @@
             label-width="7.5rem"
             style="margin-bottom: 15px"
           >
+            <el-input v-model="ruleForm.esignature" :rows="3" type="textarea" />
             <!-- :data="upLoadData" -->
-            <el-upload
+            <!-- <el-upload
               class="avatar-uploader"
               action="111"
               :show-file-list="false"
@@ -83,7 +84,7 @@
             >
               <img v-if="form.signPic" :src="form.signPic" class="avatar" />
               <i v-else class="el-icon-plus avatar-uploader-icon el-upload" />
-            </el-upload>
+            </el-upload> -->
           </el-form-item>
           <el-form-item label="头像" label-width="7.5rem">
             <!-- :data="upLoadData" -->
@@ -95,7 +96,7 @@
               :before-upload="(file) => beforeAvatarUpload(file, 'avatar')"
               style="margin-bottom: 15px"
             >
-              <img v-if="form.avatar" :src="form.avatar" class="avatar" />
+              <img v-if="form.avatar" :src="form.avatar" class="avatar">
               <i v-else class="el-icon-plus avatar-uploader-icon el-upload" />
             </el-upload>
             <div class="msg">
@@ -113,12 +114,12 @@
 </template>
 
 <script lang='ts'>
-import { Vue, Component, Prop } from "vue-property-decorator";
+import { Vue, Component, Prop } from 'vue-property-decorator'
 // import { getInfo } from '@/api/dashboard'
-import { editUser, getUserList } from "@/api/base";
-import { imageByName } from "@/api/ftp";
-import { regUserName, regPhone, regEmail } from "@/utils/reg";
-import { ElForm } from "element-ui/types/form";
+import { editUser, getUserList } from '@/api/base'
+import { imageByName } from '@/api/ftp'
+import { regUserName, regPhone, regEmail } from '@/utils/reg'
+import { ElForm } from 'element-ui/types/form'
 @Component({})
 export default class UserInfoEdit extends Vue {
   @Prop({ default: false }) showApply: boolean;
@@ -126,212 +127,212 @@ export default class UserInfoEdit extends Vue {
   difference = true;
   data = null;
   ruleForm = {
-    username: "",
+    username: '',
     // password: '',
-    realName: "",
-    phone: "",
-    email: "",
-    departmentId: "",
+    realName: '',
+    phone: '',
+    email: '',
+    departmentId: '',
     roles: [],
-    job: "",
-    note: "",
-    file: "",
-    electronicFile: "",
+    job: '',
+    note: '',
+    file: '',
+    electronicFile: ''
   };
   form = {
-    avatar: "",
-    signPic: "",
-    imageUrl:""
+    avatar: '',
+    signPic: '',
+    imageUrl: ''
   };
   rules = {
     username: [
-      { required: true, message: "请输入登录名", trigger: "blur" },
+      { required: true, message: '请输入登录名', trigger: 'blur' },
       {
         pattern: regUserName(),
-        message: "登录名只能输入英文、数字",
-        trigger: "blur",
-      },
+        message: '登录名只能输入英文、数字',
+        trigger: 'blur'
+      }
     ],
-    realName: [{ required: true, message: "请输入用户名", trigger: "blur" }],
+    realName: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
     phone: [
-      { pattern: regPhone(), message: "请输入正确联系手机号", trigger: "blur" },
+      { pattern: regPhone(), message: '请输入正确联系手机号', trigger: 'blur' }
     ],
     email: [
-      { pattern: regEmail(), message: "请输入正确邮箱", trigger: "blur" },
-    ],
+      { pattern: regEmail(), message: '请输入正确邮箱', trigger: 'blur' }
+    ]
   };
   mounted() {
-    this.getUserInfo();
+    this.getUserInfo()
   }
   /**
    * @description 获取用户信息
    */
   getUserInfo() {
-    let userId = this.$store.state.user.userId;
+    const userId = this.$store.state.user.userId
 
-    var that = this;
+    var that = this
     getUserList({ id: userId }).then((res) => {
-      console.log(res);
+      console.log(res)
       if (res.code !== -1) {
-        let result = res.result;
+        const result = res.result
         if (result.records.length > 0) {
-          that.data = result.records[0];
+          that.data = result.records[0]
           that.data.phone = that.strIsNull(that.data.phone)
-            ? ""
-            : that.data.phone; //联系电话
+            ? ''
+            : that.data.phone // 联系电话
           that.data.email = that.strIsNull(that.data.email)
-            ? ""
-            : that.data.email; //邮箱
-          that.data.job = that.strIsNull(that.data.job) ? "" : that.data.job; //工作岗位
-          that.data.note = that.strIsNull(that.data.note) ? "" : that.data.note; //工作职责
-          that.handleEdit();
+            ? ''
+            : that.data.email // 邮箱
+          that.data.job = that.strIsNull(that.data.job) ? '' : that.data.job // 工作岗位
+          that.data.note = that.strIsNull(that.data.note) ? '' : that.data.note // 工作职责
+          that.handleEdit()
         } else {
-          that.$message.error("未查询到相关的用户信息");
+          that.$message.error('未查询到相关的用户信息')
         }
       } else {
-        //@ts-ignore
-        that.$message.error(res.$message);
+        // @ts-ignore
+        that.$message.error(res.$message)
       }
-    });
+    })
   }
 
   // 上传之前的钩子函数
   beforeAvatarUpload(file, type) {
-    const isJPG = file.type === "image/jpeg";
-    const isPng = file.type === "image/png";
-    const isLt2M = file.size / 1024 / 1024 < 2;
+    const isJPG = file.type === 'image/jpeg'
+    const isPng = file.type === 'image/png'
+    const isLt2M = file.size / 1024 / 1024 < 2
 
     if (!isJPG && !isPng) {
-      this.$message.error("上传图片只能是 JPG或png 格式!");
-      return false;
+      this.$message.error('上传图片只能是 JPG或png 格式!')
+      return false
     }
     if (!isLt2M) {
-      this.$message.error("上传头像图片大小不能超过 2MB!");
-      return false;
+      this.$message.error('上传头像图片大小不能超过 2MB!')
+      return false
     }
     // console.log('2222', file)
-    type === "avatar"
+    type === 'avatar'
       ? (this.ruleForm.file = file)
-      : (this.ruleForm.electronicFile = file);
-    const _this = this;
-    const reader = new FileReader();
+      : (this.ruleForm.electronicFile = file)
+    const _this = this
+    const reader = new FileReader()
     // 转base64
-    reader.onload = function (e) {
-      type === "sign"
+    reader.onload = function(e) {
+      type === 'sign'
         ? (_this.form.signPic = e.target.result.toString())
-        : (_this.form.avatar = e.target.result.toString()); // 将图片路径赋值给src
-    };
-    reader.readAsDataURL(file);
-    return false; // 屏蔽了action的默认上传
+        : (_this.form.avatar = e.target.result.toString()) // 将图片路径赋值给src
+    }
+    reader.readAsDataURL(file)
+    return false // 屏蔽了action的默认上传
   }
   // 图片上传成功实现本地预览
   handleAvatarSuccess(res, file) {
-    this.form.imageUrl = URL.createObjectURL(file.raw); // 表单获取到提交时要传的图片路径
+    this.form.imageUrl = URL.createObjectURL(file.raw) // 表单获取到提交时要传的图片路径
   }
 
   // 拼接数据
   readyData() {
-    const data = new FormData();
+    const data = new FormData()
     // data.append('file', this.ruleForm.file)// 图片
     // 传其他参数
     for (const key in this.ruleForm) {
-      if (key === "roles") {
-        data.append(key, this.ruleForm[key].toString());
+      if (key === 'roles') {
+        data.append(key, this.ruleForm[key].toString())
       } else if (
-        key === "phone" ||
-        key === "email" ||
-        key === "job" ||
-        key === "note"
+        key === 'phone' ||
+        key === 'email' ||
+        key === 'job' ||
+        key === 'note'
       ) {
-        let tempVal = this.ruleForm[key] + "";
+        let tempVal = this.ruleForm[key] + ''
         if (this.strIsNull(tempVal)) {
-          tempVal = "null";
+          tempVal = 'null'
         }
-        data.append(key, tempVal);
+        data.append(key, tempVal)
       } else {
-        data.append(key, this.ruleForm[key]);
+        data.append(key, this.ruleForm[key])
       }
     }
     // 编辑添加id
-    data.append("id", this.data.id);
-    return data;
+    data.append('id', this.data.id)
+    return data
   }
   // 提交
   submitForm() {
     const _this = this;
     (this.$refs.ruleForm as ElForm).validate((valid) => {
       if (valid) {
-        const data = _this.readyData();
-        _this.editSubmit(data);
+        const data = _this.readyData()
+        _this.editSubmit(data)
       } else {
-        return false;
+        return false
       }
-    });
+    })
   }
 
   // 修改提交
   editSubmit(data) {
-    const _this = this;
+    const _this = this
     editUser(data).then((res) => {
       if (res.code !== -1) {
         if (res.result.code === -1) {
-          this.$message.error(res.result.message);
+          this.$message.error(res.result.message)
         } else {
-          _this.$message.success("修改成功");
-          _this.closeDialog();
+          _this.$message.success('修改成功')
+          _this.closeDialog()
         }
       }
-    });
+    })
   }
 
   // 编辑参数回显
   handleEdit() {
-    this.ruleForm = this.assignment(this.ruleForm, this.data) as any;
-    const arr = [this.data.avatar, this.data.esignature];
+    this.ruleForm = this.assignment(this.ruleForm, this.data) as any
+    const arr = [this.data.avatar, this.data.esignature]
     arr.forEach((item, index) => {
-      if (item === null) return;
+      if (item === null) return
       imageByName(item).then((res) => {
         if (res.status === 200) {
           index === 1
             ? (this.form.signPic = res.config.url)
-            : (this.form.avatar = res.config.url);
+            : (this.form.avatar = res.config.url)
         }
-      });
-    });
+      })
+    })
   }
 
   // 赋值
   assignment(par, role) {
-    const data = {};
+    const data = {}
     for (const key in par) {
-      if (key === "roles") {
+      if (key === 'roles') {
         data[key] =
           role.roleIdList &&
           role.roleIdList != null &&
-          role.roleIdList.split(",");
+          role.roleIdList.split(',')
       } else {
-        data[key] = role[key];
+        data[key] = role[key]
       }
     }
-    return data;
+    return data
   }
 
   // 关闭dialog
   closeDialog() {
-    this.$emit("update:showApply", false);
+    this.$emit('update:showApply', false)
   }
 
   /*
    * @description 判断字符串是否为空
    */
   strIsNull(strVal) {
-    strVal = strVal || "";
+    strVal = strVal || ''
     return (
-      typeof strVal == "undefined" ||
+      typeof strVal === 'undefined' ||
       strVal == null ||
-      strVal == "" ||
-      strVal == "null"
-    );
+      strVal == '' ||
+      strVal == 'null'
+    )
   }
 }
 </script>
