8f6e255256be852e7ba02710bb59ed310e5b0a53
yangjing <504259548@qq.com>
2022-05-07 18:31:10 +0800

    - 地图组件

---
+++ src/components/SimpleMap/index.vue
@@ -0,0 +1,85 @@
+<template>
+  <div ref="mainMap" class="map-container" :style="mapSize"></div>
+</template>
+
+<script>
+import { appconfig } from "staticPub/config";
+import "ol/ol.css";
+import Map from "ol/Map";
+import View from "ol/View";
+import TileLayer from "ol/layer/Tile";
+import { Logo, TileSuperMapRest } from "@supermap/iclient-ol";
+import { TF_Layer } from '@/views/zhpt/common/mapUtil/layer'
+import { defaults as controls } from 'ol/control'
+
+export default {
+    name: "simpleMap",
+    props: {
+        mapWidth: {
+            default: 100,
+            type: Number
+        },
+        mapHeight: {
+            default: 100,
+            type: Number
+        }
+    },
+
+    data () {
+        return {
+
+        }
+    },
+    methods: {
+        initMap() {
+            let { initCenter, initZoom } = appconfig
+            let layerResource = appconfig.gisResource['iserver_resource'].layerService.layers
+            let map = new Map({
+                target: this.$refs.mainMap,
+                controls: controls({
+                    zoom: false
+                }),
+                view: new View({
+                    center: initCenter,
+                    zoom: initZoom,
+                    maxZoom: 20,
+                    minZoom: 5,
+                    projection: 'EPSG:4326'
+                })
+            })
+            this.map = map
+
+            layerResource.forEach(layerConfig => {
+                let { name, type, url, parentname, id, visible = true } = layerConfig
+                let layer = new TF_Layer().createLayer({ url, type, visible, properties: { id, name, parentname } })
+                this.map.addLayer(layer)
+            })
+        },
+        // 加入缺陷数据
+        addData () {
+            
+        },
+    },
+
+    created () {},
+
+    mounted () {
+        console.log('加载地图')
+        this.initMap()
+    },
+
+    destroyed () {},
+
+    computed: {
+        mapSize () {
+            return { height: `${this.mapHeight}%`, width: `${this.mapWidth}%` }
+        }
+    }
+}
+</script>
+
+<style lang='scss' scoped>
+    .map-container {
+        display: relative;
+    }
+</style>
