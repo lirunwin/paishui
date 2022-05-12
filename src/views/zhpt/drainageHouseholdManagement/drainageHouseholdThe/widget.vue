36cb792fb05785941de6d11a5dc5a98fe065e0c6
xieqy <jingcheng288@gmail.com>
2022-05-12 13:40:37 +0800

    update

--- src/views/zhpt/drainageHouseholdManagement/drainageHouseholdThe/widget.vue
+++ src/views/zhpt/drainageHouseholdManagement/drainageHouseholdThe/widget.vue
@@ -8,12 +8,18 @@
         <el-form :model="visibleSettings" :rules="rules" size="mini" label-width="100px" ref="visibleSettings"> 
             <el-form-item label="项目名称：" prop="projectName">
                 <el-select v-model="visibleSettings.projectName" placeholder="请选择项目" multiple clearable>
-                    <el-option label="xxx项目" value="0"></el-option>
+                    <el-option  v-for="item of projectInfo" :key="item"
+                    :label="item" 
+                    :value="item">
+                    </el-option>
                 </el-select>
             </el-form-item>
             <el-form-item label="地址：">
                 <el-select v-model="visibleSettings.address" placeholder="请选择地址" multiple clearable>
-                    <el-option label="xxx路" value="0"></el-option>
+                    <el-option  v-for="item of addressInfo" :key="item"
+                    :label="item" 
+                    :value="item">
+                    </el-option>
                 </el-select>
             </el-form-item>
             <el-form-item label="统计范围：">
@@ -28,17 +34,17 @@
                 </el-radio-group>
                 <div class="right-group">
                     <el-tooltip content="圆形选择" placement="top" effect="light">
-                        <el-button size="mini"  plain :disabled="isCustom">
+                        <el-button size="mini"  plain :disabled="isCustom" @click="customRange('circle')">
                             <img src="../images/C.png" width="20px" height="20px" style="object-fit: contain" />
                         </el-button>
                     </el-tooltip>
                     <el-tooltip content="矩形选择" placement="top" effect="light">
-                        <el-button size="mini"  plain :disabled="isCustom">
+                        <el-button size="mini"  plain :disabled="isCustom" @click="customRange('rect')">
                             <img src="../images/R.png" width="20px" height="20px" style="object-fit: contain" />
                         </el-button>
                     </el-tooltip>
                     <el-tooltip content="多边形选择" placement="top" effect="light">
-                    <el-button size="mini"  plain :disabled="isCustom">
+                    <el-button size="mini"  plain :disabled="isCustom" @click="customRange('polygon')">
                         <img src="../images/P.png" width="20px" height="20px" style="object-fit: contain" />
                     </el-button>
                     </el-tooltip>
@@ -72,13 +78,13 @@
         <div class="titleName">专题图列表</div>
     </div>
     <div class="thematicMapList">
-        <div v-for="(item, index) in thematicMapList" :key="index" class="thematicMap-list">
+        <div v-for="(item, index) in thematicMapList" :key="index" class="thematicMap-list" v-show="showThem">
         <div>
             <div class="thematicMap-title">
             <i
                 style="cursor: pointer"
-                @click="changeArrow(index)"
-                :class="{ 'el-icon-caret-bottom': showThemBox[index], 'el-icon-caret-right': !showThemBox[index] }"
+                @click="showThemBox=!showThemBox"
+                :class="{ 'el-icon-caret-bottom': showThemBox, 'el-icon-caret-right': !showThemBox }"
             ></i>
             <el-checkbox
                 @change="setThemLayerVisible(index, item.open)"
@@ -87,16 +93,9 @@
             ></el-checkbox>
             </div>
             <transition name="el-zoom-in-top">
-            <div v-if="item.type === 'gradient' && showThemBox[index]" class="transition-box">
-                <div class="line-color"></div>
-                <div class="text-mix-max">
-                <span>{{ item.start }}</span>
-                <span>{{ item.end }}</span>
-                </div>
-            </div>
-            <div v-else-if="showThemBox[index]" class="transition-box">
+            <div v-if="showThemBox" class="transition-box">
                 <ul>
-                <li v-for="(level, i) in item.level" :key="i" :class="comStyle(item.type, level.color)">
+                <li v-for="(level, i) in item.level" :key="i" :class="comStyle(level.color)">
                     {{ level.label + "("+level.num + level.unit +")"}}
                 </li>
                 </ul>
@@ -109,7 +108,26 @@
 </template>
 
 <script>
-import VectorSource from 'ol/source/Vector'
+import { esriConfig, appconfig } from 'staticPub/config'
+import iDraw from '@/views/zhpt/common/mapUtil/draw'
+import iQuery from '@/views/zhpt/common/mapUtil/query'
+import * as turf from '@turf/turf';
+import GeoJSON from 'ol/format/GeoJSON'
+import { Vector as VectorSource } from 'ol/source'
+import { Vector as VectorLayer } from 'ol/layer'
+import Feature from 'ol/Feature';
+import {Polygon, LineString, Point } from 'ol/geom';
+import CircleObject from 'ol/geom/Circle'
+import {fromCircle} from 'ol/geom/Polygon';
+import {
+  Style,
+  Circle,
+  Icon,
+  Fill,
+  RegularShape,
+  Stroke,
+  Text
+} from 'ol/style'
 export default {
     name:"drainageHouseholdThe",//排放口专题图
     data(){
@@ -117,9 +135,11 @@ export default {
             visibleSettings:{
                 projectName:"",
                 address:"",
-                statisticRange:"",
+                statisticRange:"all",
                 region:""
             },
+            projectInfo:[],//项目信息
+            addressInfo:[],//地址信息
             rules:{
                 projectName: [
                     { required: true, message: '请选择项目', trigger: 'blur' },
@@ -128,24 +148,38 @@ export default {
             isCustom:true,
             isRegion:true,
             regionType:"按行政区",
-
-            //
             thematicMapList: [
                 {
                     title: '排放口类型专题图',
                     layerName: 'pipeDefectLayer',
-                    open: false,
+                    open: true,
                     type: 'circle',
                     level: [
-                        { color: 'blue', label: '雨水', num: 111, unit: '个' },
-                        { color: 'red', label: '污水', num: 111, unit: '个' },
-                        { color: 'brown', label: '雨污合流', num: 111, unit: '个' }
+                        { color: 'blue', label: '雨水', num: 0, unit: '个' },
+                        { color: 'red', label: '污水', num: 0, unit: '个' },
+                        { color: 'brown', label: '雨污合流', num: 0, unit: '个' }
                     ]
                 },
             ],
-            showThemBox: [true, true, true, true],
-            outletLayer:null,
+            showThem:false,
+            showThemBox:true,
+            //地图要素变量
+            drawer:null,
+            customFeature:null,
+            limitFeature:null,
+            ysFeatures:[],
+            wsFeatures:[],
+            yswsFeatures:[],
+            ysLayer:null,
+            wsLayer:null,
+            yswsLayer:null,
+            dataSetInfo:null,//查询数据集
         }
+    },
+    mounted(){
+        this.view=this.$attrs.data.mapView
+        this.getDataSet();
+        this.getSelectInfo();
     },
     watch:{
         'visibleSettings.statisticRange':{
@@ -153,14 +187,61 @@ export default {
                 this.isCustom=(n=="custom")?false:true
                 this.isRegion=(n=="region")?false:true
             }
+        },
+        'thematicMapList':{
+            handler(n,o){
+                if(n[0].open){
+                    this.ysLayer.setVisible(true)
+                    this.wsLayer.setVisible(true)
+                }else{
+                    this.ysLayer.setVisible(false)
+                    this.wsLayer.setVisible(false)
+                }
+            },
+            deep:true
         }
     },
     methods:{
+        setThemLayerVisible(){
+            
+        },
+        getDataSet(){
+            let dataService = appconfig.gisResource['iserver_resource'].dataService
+            this.dataSetInfo = dataService.dataSetInfo.filter(info => info.label === '排放口')
+            console.log("数据服务",this.dataSetInfo)
+        },
+        //获取项目以及地址下拉框内容
+        getSelectInfo(){
+            let queryTask = new iQuery({ dataSetInfo:this.dataSetInfo })
+            let queryText = `SMID >= 0 `
+            queryTask.sqlQuery(queryText).then(resArr => {
+                let features = []
+                resArr.forEach(item => {
+                    if (item && item.result.featureCount !== 0) {
+                        features.push(item.result.features)
+                    }
+                })
+                if(features.length==0) return
+                this.assembleSelectInfo(features[0].features)
+            }).catch(err=>{
+                console.log(err)
+            })
+        },
+        //组装下拉框信息
+        assembleSelectInfo(featuresInfo){
+            featuresInfo.forEach(item=>{
+                let {PRJ_NAME,ADDRESS} = item.properties
+                this.projectInfo.push(PRJ_NAME)
+                this.addressInfo.push(ADDRESS)
+            })
+            this.projectInfo=[...new Set(this.projectInfo)]
+            this.addressInfo=[...new Set(this.addressInfo)]
+        },
         //专题图查看
         viewThematicMap(){
             this.$refs['visibleSettings'].validate((valid) => {
                 if (valid) {
-                    
+                    this.checkResult();
                 } else {
                     return false;
                 }
@@ -170,30 +251,189 @@ export default {
         handleCommand(command){
             this.regionType=command
         },
-        changeArrow(index) {
-            this.$set(this.showThemBox, index, !this.showThemBox[index])
+        comStyle(color) {
+            return `item-${color} type-circle`
         },
-        setThemLayerVisible(index, visible) {
-            this.outletLayer = new VectorLayer({ source: new VectorSource(), visible: false })
-            this.outletLayer.setVisible(visible)
-            visible && this.openDefect()
+        //查看结果
+        checkResult(){
+            this.removeLayer();
+            switch(this.visibleSettings.statisticRange){
+                case 'all':this.limitFeature=null;
+                            break
+                case 'map': this.mapRange();
+                            break
+                case 'custom': this.limitFeature=this.customFeature;
+                            break
+            }
+            this.queryTerm();
         },
-        comStyle(type, color) {
-        let className = ''
-        switch (type) {
-            case 'circle':
-            className = 'type-circle'
-            break
-            case 'square':
-            className = 'type-square'
-            break
-            case 'line':
-            className = 'type-line'
-            break
-        }
-        return `item-${color} ${className}`
+        //地图范围
+        mapRange(){
+            //地图范围
+            let size =this.view.getSize()
+            let extent = this.view.getView().calculateExtent(size)
+            //创建Feature，并添加进矢量容器中
+            let feature = new Feature({
+                geometry: new Polygon([[[extent[0], extent[1]], [extent[0], extent[3]], [extent[2], extent[3]], [extent[2], extent[1]],[extent[0], extent[1]]]]),
+                name: 'mapRange'
+            });
+            this.limitFeature=feature
+        },
+        //自定义范围
+        customRange(type){
+            this.drawer && this.drawer.end()
+            this.showThem=false
+            this.removeLayer()
+            // this.queryLayer && this.queryLayer.getSource().clear()
+            this.drawer = new iDraw(this.view, type, {
+                endDrawCallBack: feature => {
+                    this.drawer.remove()
+                    this.customFeature=feature
+                },
+                showCloser: true,
+                drawStyle:{
+                    pointSize:5,
+                    pointColor:'rgb(246,82,82)',
+                    lineWidth:2,
+                    lineColor:'rgb(246,82,82)',
+                    fillColor:'rgba(246,82,82,0.1)',
+                    lineDash:[0,0]
+                }
+            })
+            this.drawer.start()
+        },
+        //条件查询
+        queryTerm(){
+            let prjName=[];
+            let address=[];
+            this.visibleSettings.projectName.forEach(item=>{
+                prjName.push(`PRJ_NAME = '${item}'`)
+            })
+            this.visibleSettings.address.forEach(item=>{
+                address.push(`ADDRESS = '${item}'`)
+            })
+            prjName=prjName.join(' or ')
+            address=address.join(' or ')
+            let queryTask = new iQuery({ dataSetInfo:this.dataSetInfo })
+            let queryText = `PRJ_NAME = "" or ${address}`
+            console.log(queryText)
+            queryTask.sqlQuery(queryText).then(resArr => {
+                console.log("条件查询结果",resArr)
+                let featruesData = resArr.filter(item => {
+                    return item.result.featureCount !== 0
+                })
+                // 数据过滤
+                if (featruesData.length !== 0) {
+                    this.filterFeatures(featruesData)
+                }else{
+                    this.showThem=false;
+                    this.$message('暂无数据！')
+                    return
+                }
+            }).catch(err=>{
+                console.log(err)
+            })
+        },
+        filterFeatures(featruesData){
+            // console.log(featruesData)
+            featruesData.forEach(featrueObj => {
+                let features = featrueObj.result.features.features
+                let themFeatures = features.map(feature => new GeoJSON().readFeature(feature))
+                // 范围限制
+                if (this.limitFeature) {
+                    let limitGeometry 
+                    if(this.limitFeature.getGeometry() instanceof CircleObject){
+                        let polygon = fromCircle(this.limitFeature.getGeometry())
+                        limitGeometry=turf.polygon(polygon.getCoordinates())
+                    }else{
+                        limitGeometry = turf.polygon(this.limitFeature.getGeometry().getCoordinates())
+                    }
+                    themFeatures = themFeatures.filter(feature => {
+                        let geomtry = feature.getGeometry(), inGeometry
+                        if (geomtry instanceof Point) {
+                            inGeometry = turf.point(geomtry.getCoordinates())
+                        } else if (geomtry instanceof LineString) {
+                            inGeometry = turf.lineString(geomtry.getCoordinates())
+                        } else return false
+                        return turf.booleanContains(limitGeometry, inGeometry)
+                    })
+                }
+                this.showTematicMap(themFeatures)
+            })
+        },
+        showTematicMap(themFeatures){
+            if(themFeatures.length==0){
+                this.showThem=false;
+                this.$message('暂无数据！')
+                return
+            }
+            this.ysFeatures=[];
+            this.wsFeatures=[];
+            this.yswsFeatures=[];
+            themFeatures.forEach(feaJson => {
+                this.classification(feaJson)
+            })
+            this.initLayer();
+            this.showThem=true;
+            this.thematicMapList[0].level.map(item=>{
+                if(item.label=='雨水') return item.num=this.ysFeatures.length
+                if(item.label=='污水') return item.num=this.wsFeatures.length
+            })
+        },
+        classification(features){
+            switch(features.values_.TYPE){
+                case '雨水':this.ysFeatures.push(features)
+                            break
+                case '污水':this.wsFeatures.push(features)
+                            break
+            }
+        },
+        // 初始化图层
+        initLayer() {
+            this.ysLayer = new VectorLayer({
+                source: new VectorSource(),
+                style: new Style({
+                    // 将点设置成圆形样式
+                    image: new Circle({
+                        // 点的颜色
+                        fill: new Fill({
+                            color: 'blue'
+                        }),
+                        // 圆形半径
+                        radius: 5
+                    }),
+                })
+            })
+            this.wsLayer = new VectorLayer({
+                source: new VectorSource(),
+                style: new Style({
+                    // 将点设置成圆形样式
+                    image: new Circle({
+                        // 点的颜色
+                        fill: new Fill({
+                            color: 'red'
+                        }),
+                        // 圆形半径
+                        radius: 5
+                    }),
+                })
+            })
+            this.view.addLayer(this.ysLayer)
+            this.view.addLayer(this.wsLayer)
+            this.ysLayer.getSource().addFeatures(this.ysFeatures)
+            this.wsLayer.getSource().addFeatures(this.wsFeatures)
+        },
+        //删除
+        removeLayer(){
+            this.view.removeLayer(this.ysLayer)
+            this.view.removeLayer(this.wsLayer)
+            this.view.removeLayer(this.yswsLayer)
         },
     },
+    beforeDestroy(){
+        this.removeLayer()
+        if(this.drawer) this.drawer.clear()
+    }
 }
 </script>
 
@@ -224,11 +464,11 @@ export default {
         //     width: 100px;
         //     text-align: right;
         // }
-        /deep/.el-form-item__content{
-            // display: flex;
-            // float:left
-            width: 260px;
-        }
+        // /deep/.el-form-item__content{
+        //     // display: flex;
+        //     // float:left
+        //     width: 260px;
+        // }
         .right-group{
             padding-left: 20px;
             margin-top: -8px;
