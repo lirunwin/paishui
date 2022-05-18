93481f00a885c504f1cde040aa7384801a7ed336
LR <lr@somemailserver.com>
2022-05-18 14:41:50 +0800

    add pagination

--- src/views/monitoring/components/BaseTable/index.vue
+++ src/views/monitoring/components/BaseTable/index.vue
@@ -1,20 +1,35 @@
 <template>
-  <el-table class="base-table" v-on="$listeners" v-bind="{ ...defaultAttrs, ...attrs }" :style="style">
-    <template slot="empty">
-      <img src="@/assets/icon/null.png" alt="" />
-      <p style="margin-top:-7px">暂无数据</p>
-    </template>
-    <template v-for="{ prop, type, _slot, ...col } of columns">
-      <template v-if="_slot">
-        <el-table-column :key="prop || type" v-bind="{ align: 'center', prop, type, ...col }">
-          <template slot-scope="slotScope" v-if="_slot">
-            <slot :name="`${prop}-${slotScope.$index}`" v-bind="{ ...slotScope, col: { prop, type, _slot, ...col } }" />
-          </template>
-        </el-table-column>
+  <div class="base-table">
+    <el-table class="table" v-on="$listeners" v-bind="{ ...defaultAttrs, ...attrs }" :style="style">
+      <template slot="empty">
+        <img src="@/assets/icon/null.png" alt="" />
+        <p style="margin-top:-7px">暂无数据</p>
       </template>
-      <el-table-column v-else :key="prop || type" v-bind="{ align: 'center', prop, type, ...col }" />
-    </template>
-  </el-table>
+      <template v-for="{ prop, type, _slot, ...col } of columns">
+        <template v-if="_slot">
+          <el-table-column :key="prop || type" v-bind="{ align: 'center', prop, type, ...col }">
+            <template slot-scope="slotScope" v-if="_slot">
+              <slot
+                :name="`${prop}-${slotScope.$index}`"
+                v-bind="{ ...slotScope, col: { prop, type, _slot, ...col } }"
+              />
+            </template>
+          </el-table-column>
+        </template>
+        <el-table-column v-else :key="prop || type" v-bind="{ align: 'center', prop, type, ...col }" />
+      </template>
+    </el-table>
+    <el-pagination
+      v-if="pagination"
+      :current-page="pagination.current"
+      :page-sizes="[10, 20, 30, 50, 100, 1000]"
+      :page-size="pagination.size"
+      :total="pagination.total"
+      layout="total, sizes, prev, pager, next, jumper"
+      @size-change="$emit('sizeChange', $event)"
+      @current-change="$emit('currentChange', $event)"
+    />
+  </div>
 </template>
 
 <script lang="ts">
@@ -22,10 +37,17 @@ import { ElTable } from 'element-ui/types/table'
 import { ElTableColumn } from 'element-ui/types/table-column'
 import Vue, { PropType } from 'vue'
 
+interface IPagination {
+  current?: string | number
+  size?: string | number
+  total?: string | number
+}
+
 export default Vue.extend({
   inheritAttrs: false,
   props: {
-    columns: { type: Array as PropType<Partial<ElTableColumn>[]> }
+    columns: { type: Array as PropType<Partial<ElTableColumn>[]> },
+    pagination: { type: Object as PropType<IPagination> }
   },
   data() {
     return {
@@ -65,15 +87,39 @@ export default Vue.extend({
   }
 })
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
 .base-table {
-  &.el-table--striped {
-    /deep/ .el-table__body tr.el-table__row--striped.el-table__row--striped.el-table__row--striped td {
-      background-color: #f3f7fe;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  .table {
+    flex: 1 1 auto;
+    margin-bottom: 14px;
+    &.el-table--striped {
+      /deep/ .el-table__body tr.el-table__row--striped.el-table__row--striped.el-table__row--striped td {
+        background-color: #f3f7fe;
+      }
+    }
+    /deep/ td.el-table__cell {
+      border-color: rgba(#dedede, 0.6);
     }
   }
-  /deep/ td.el-table__cell {
-    border-color: rgba(#dedede, 0.6);
+}
+
+.page-container {
+  display: flex;
+  flex-direction: column;
+  > .el-row {
+    height: 100%;
+    > .el-col {
+      height: 100%;
+    }
+  }
+
+  .table-container {
+    flex: 1 1 100%;
+    height: 100%;
   }
 }
 </style>
