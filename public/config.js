e4f1e3c8d9daad34a478df2b65ea8d5a380a0d68
李顺 <876330731@qq.com>
2022-06-23 17:16:46 +0800

    iserver地址

--- public/config.js
+++ public/config.js
@@ -9,7 +9,7 @@ export const esriConfig = {
 
 export const iserverConfig = {
   // 'baseUrl': 'http://117.174.10.73:8090/iserver/'
-  'baseUrl': 'http://192.168.2.238:8090/iserver/'
+  'baseUrl': 'http://221.182.8.141:8090/iserver/'
 }
 
 export const appconfig = {
